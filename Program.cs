5625ce36ffb4e6c21f51c4e19283bb96c92d6b53
Mauricio Corte <Corte.Mauricio98@gmail.com>
2024-09-18 15:28:34 +0200

    Add new text to second WriteLine statement

--- Program.cs
+++ Program.cs
@@ -4,7 +4,7 @@ class Program
 {
     static void Main(string[] args) {
         Console.WriteLine("Hello, World!");
-        Console.WriteLine("Hello, Man!");
+        Console.WriteLine("Hello, Man! Hello!");
         Console.WriteLine("Hello, World! Hello!");
     }
 }
