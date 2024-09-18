2564613ed4c94a11664c427756f4640fc0b966e1
Mauricio Corte <Corte.Mauricio98@gmail.com>
2024-09-18 16:05:05 +0200

    Add method to get user number

--- Program.cs
+++ Program.cs
@@ -3,7 +3,21 @@
 class Program
 {
     static void Main(string[] args)
-    { 
+    {
+        int GetNumber()
+        {
+            int userNumber;
+            bool isNumber;
+            do
+            {
+                Console.Clear();
+                Console.WriteLine("Please enter an integer between 1 and 20.");
+                isNumber = int.TryParse(Console.ReadLine(), out userNumber);
+            } while (!isNumber || userNumber > 20 || userNumber < 0);
+
+            return userNumber;
+        }
+        
         int GetRandomNumber()
         {
             Random random = new Random();
@@ -15,5 +29,7 @@ class Program
         {
             return userNumber == randomlyGeneratedNumber ? true : false;
         }
+        
+        
     }
 }
